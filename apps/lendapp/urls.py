9f835abf6ee0f043522843f8da1f675c3d7e6572
Kamil Cholipski <8065079-kamil.cholipski@users.noreply.gitlab.com>
2021-04-09 14:00:17 +0200

    Standardize endpoint names

--- apps/lendapp/urls.py
+++ apps/lendapp/urls.py
@@ -5,12 +5,12 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('ItemCategory/', views.ItemCategoryList.as_view(), name=views.ItemCategoryList.name),
-    path('ItemCategory/<int:pk>/', views.ItemCategoryDetail.as_view(), name=views.ItemCategoryDetail.name),
-    path('LoanStatus/', views.LoanStatusList.as_view(), name=views.LoanStatusList.name),
-    path('LoanStatus/<int:pk>/', views.LoanStatusDetail.as_view(), name=views.LoanStatusDetail.name),
-    path('Loan/', views.LoanList.as_view(), name=views.LoanList.name),
-    path('Loan/<int:pk>/', views.LoanDetail.as_view(), name=views.LoanDetail.name),
-    path('return/', views.ReturnLend.as_view(), name="Return a loan"),
+    path('category/', views.ItemCategoryList.as_view(), name=views.ItemCategoryList.name),
+    path('category/<int:pk>', views.ItemCategoryDetail.as_view(), name=views.ItemCategoryDetail.name),
+    path('loan-status/', views.LoanStatusList.as_view(), name=views.LoanStatusList.name),
+    path('loan-status/<int:pk>', views.LoanStatusDetail.as_view(), name=views.LoanStatusDetail.name),
+    path('item-loan/', views.LoanList.as_view(), name=views.LoanList.name),
+    path('item-loan/<int:pk>', views.LoanDetail.as_view(), name=views.LoanDetail.name),
+    path('item-return/', views.ReturnLend.as_view(), name="Return a loan"),
     path('', views.ApiRoot.as_view(), name=views.ApiRoot.name),
 ]
--- lend_and_forget/urls.py
+++ lend_and_forget/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('lend/', include('apps.lendapp.urls')),
-    path('', include('apps.authorization.urls')),
+    path('lend-app/', include('apps.lendapp.urls')),
+    path('auth/', include('apps.authorization.urls')),
 ]
